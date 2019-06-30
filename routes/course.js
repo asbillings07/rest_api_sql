d38ac086d3a282484739854465442bac26d3334f
Aaron Billings <asbillings2007@gmail.com>
2019-06-30 15:05:52 -0400

    add GET course routes

--- routes/course.js
+++ routes/course.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const Course = require('../models').Course;
+const User = require('../models').User;
 
 // middleware error handler
 
@@ -17,10 +18,41 @@ function asyncHandler(cb) {
 // Course Routes
 
 //GET /api/courses 200 - Returns a list of courses (including the user that owns each course)
-router.get('/courses', (req, res, next) => {});
+router.get(
+  '/courses',
+  asyncHandler(async (req, res, next) => {
+    const courses = await Course.findAll({
+      include: [
+        {
+          model: User,
+          as: 'user',
+          attributes: { exclude: ['password', 'createdAt', 'updatedAt'] },
+        },
+      ],
+      attributes: { exclude: ['password', 'createdAt', 'updatedAt'] },
+    });
+    res.status(200).json(courses);
+  })
+);
 
 //GET /api/courses/:id 200 - Returns a the course (including the user that owns the course) for the provided course ID
-router.get('/courses/:id', (req, res, next) => {});
+router.get('/courses/:id', async (req, res, next) => {
+  const id = req.params.id;
+  const course = await Course.findAll({
+    where: {
+      id,
+    },
+    include: [
+      {
+        model: User,
+        as: 'user',
+        attributes: { exclude: ['password', 'createdAt', 'updatedAt'] },
+      },
+    ],
+    attributes: { exclude: ['password', 'createdAt', 'updatedAt'] },
+  });
+  res.status(200).json(course);
+});
 
 //POST /api/courses 201 - Creates a course, sets the Location header to the URI for the course, and returns no content
 router.post('/courses', (req, res, next) => {});
